0d804a90bc638c7d6e7d6e69833039599812c5d4
Vinicius Santiago <vinicius_quartz@hotmail.com>
2022-07-28 19:45:53 -0300

    edit pipelines

--- iac/terraform/provider.tf
+++ iac/terraform/provider.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.14.0"
+      version = "~>2.0"
     }
   }
+  required_version = ">=0.14"
 }
 
 provider "azurerm" {
